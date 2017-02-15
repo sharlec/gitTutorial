05bfbcf35d1fc7757e8f690be93c29d7f4275e3b
sharlec <schan543@uwo.ca>
2017-02-14 20:41:10 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/2/14 0014.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = descriptioin;
+    }
+}
